27a23504071ce9be58dea88a5ba1b8bcb12a105a
Bonnie Tang <bonnietang725@gmail.com>
2020-01-29 13:58:21 -0800

    bonnie

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.test.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    Button btn2 = (Button) findViewById(R.id.button2);
+
+    btn2.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+}
+
