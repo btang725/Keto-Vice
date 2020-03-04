360594b2a68481114b0a4d8b77aaafc1cd1ff638
Bonnie Tang <bonnietang725@gmail.com>
2020-03-04 14:21:05 -0800

    Working on Firebase to Sign Up

--- app/src/main/java/com/example/test/SignupActivity.java
+++ app/src/main/java/com/example/test/SignupActivity.java
@@ -8,15 +8,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.test.ui.login.LoginActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//import User class HERE
+import com.example.test.User;
 
 public class SignupActivity extends AppCompatActivity {
 
     Spinner spinner_s;
-    String gender[] = {"Male", "Female"};
+    String[] gender = {"Male", "Female"};
     ArrayAdapter<String>arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +45,43 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        // TODO: UNCOMMENT THE LINE BELOW TO LINK SIGN UP BUTTON TO HOME PAGE
-//        Button btn1 = (Button) findViewById(R.id.signup_btn);
-//        btn1.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                startActivity(new Intent(SignupActivity.this, Home.class));
-//            }
-//        });
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Go to Home Class after clicking "Sign Up"
+        //TODO: go to recommendation instead
+        Button btn1 = (Button) findViewById(R.id.signup_btn);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SignupActivity.this, Home.class));
+
+                EditText et_name = (EditText)findViewById(R.id.editText);
+                EditText et_age = (EditText)findViewById(R.id.editText2);
+                EditText et_height = (EditText)findViewById(R.id.editText3);
+                EditText et_weight = (EditText)findViewById(R.id.editText4);
+                EditText et_mail = (EditText)findViewById(R.id.editText5);
+                EditText et_pass = (EditText)findViewById(R.id.editText6);
+
+                String name = et_name.getText().toString();
+                String age = et_age.getText().toString();
+                String height = et_height.getText().toString();
+                String weight = et_weight.getText().toString();
+                String mail = et_mail.getText().toString();
+                String pass = et_pass.getText().toString();
+
+                String gd = spinner_s.getSelectedItem().toString();
+
+//                 TODO: Fix this .. does userID need to be a String?
+//                private void writeNewUser(int userID, String name, String age, String height, String weight, String gd, String mail, String pass){
+                    int userID = 0;
+                    User u = new User(userID, name, age, height, weight, gd, mail, pass);
+                    mDatabase.child("users").child(userID).setValue(u);
+                    userID = userID + 1;
+//                }
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/test/User.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+public class User {
+    public int userID;
+    public String name;
+    public String age;
+    public String height;
+    public String weight;
+    public String gender;
+    public String email;
+    public String password;
+
+    public User(int userID, String name, String age, String height, String weight, String gd, String mail, String pass){
+
+    }
+
+    public User(int userID, String name, String age, String height, String weight, String gender, String email, String password){
+        this.userID = userID;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+        this.email = email;
+        this.password = password;
+    }
+
+}
