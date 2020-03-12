17351675be415dc6677396d24e024a8953ef16c8
Nanonaitor <vnanoc@hotmail.com>
2020-03-12 05:30:12 -0700

    Calendar + Recomendation Update
    - Expanded on Calendar Activity
    - Added new button backgrounds
    - Improved/changed GUI across most activities
    - Made progress on Recommendation Activity and retrieving from FDA API (needs testing and parsing)

--- app/src/main/java/com/example/test/CalendarActivity.java
+++ app/src/main/java/com/example/test/CalendarActivity.java
@@ -1,14 +1,44 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+    Button bChckRec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+        bChckRec = (Button) findViewById(R.id.buttonCheckRecommendation);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = month + "/" + (dayOfMonth + 1) + "/" + year;
+                myDate.setText(date);
+            }
+        });
+
+        bChckRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "You clicked a button, congratulations!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/test/RecommendationActivity.java
+++ app/src/main/java/com/example/test/RecommendationActivity.java
@@ -2,13 +2,114 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RecommendationActivity extends AppCompatActivity {
+    String fdaJson;
+    Button buttonDummy;
+    TextView txtJson;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
+
+        fdaJson = "https://api.fda.gov/drug/event.json?limit=1"; //Dummy for now?
+        //   https://stackoverflow.com/questions/33229869/get-json-data-from-url-using-android
+        buttonDummy = (Button) findViewById(R.id.buttonDummy);
+        Toast.makeText(getApplicationContext(), "I fear nothing is here. Better bust that rear before I get near.", Toast.LENGTH_SHORT).show();
+
+        buttonDummy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute(fdaJson); //YOUR URL ADDRESS HERE!!!
+                Toast.makeText(getApplicationContext(), "You clicked a button, congratulations!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //Just copy pasted everything below for now...
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(RecommendationActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            txtJson.setText(result);
+        }
     }
 }
